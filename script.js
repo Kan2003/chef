be8be0663cdb1620d52fc4ba350aa4d2e698f485
Kan2003 <122142009+Kan2003@users.noreply.github.com>
2023-10-25 20:42:28 +0530

    new1 commit

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.imageMasker(".home-img img" , {
+  mouseFollower: true,
+  text: "Kanha",
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
 });
 
-menu.onclick = () => {
-  navbar.classList.toggle("active");
-};
-window.onscroll = () => {
-  navbar.classList.remove("active");
-};
 
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
 
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-  }
-};
+function night(){
+  let header = document.querySelector("header");
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+  
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+  });
+  
+  menu.onclick = () => {
+    navbar.classList.toggle("active");
+  };
+  window.onscroll = () => {
+    navbar.classList.remove("active");
+  };
+  
+  // Dark Mode / light mode
+  let darkmode = document.querySelector("#darkmode");
+  
+  darkmode.onclick = () => {
+    if (darkmode.classList.contains("bx-moon")) {
+      darkmode.classList.replace("bx-moon", "bx-sun");
+      document.body.classList.add("active");
+    } else {
+      darkmode.classList.replace("bx-sun", "bx-moon");
+      document.body.classList.remove("active");
+    }
+  };
+}
+
+night();
 
 
 
